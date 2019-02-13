99e93647981681625b37c81eb18d607fcef865ed
Cassiano de Souza <redhotcassiano@gmail.com>
2019-02-12 22:56:36 -0200

    iniciando estudos redes neurais.

---
+++ perceptron1.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Este é um arquivo de script temporário.
+"""
+
+entradas = [-1, 7, 5]
+pesos = [0.8, 0.1, 0]
+
+def soma(e, p):
+    s = 0
+    for i in range(3):
+        #print(entradas[i])
+        #print(pesos[i])
+        
+        s += e[i] * p[i]
+    return s
+
+s = soma(entradas, pesos)
+
+def stepFunction(soma):
+    if (soma >= 1):
+        return 1
+    return 0
+
+r = stepFunction(s)
---
+++ perceptron2.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 12 22:25:44 2019
+
+@author: Cassiano
+"""
+import numpy as np
+
+entradas = np.array([1, 7, 5])
+pesos = np.array([0.8, 0.1, 0])
+
+def soma(e, p):    
+    return e.dot(p)
+
+s = soma(entradas, pesos)
+
+def stepFunction(soma):
+    if (soma >= 1):
+        return 1
+    return 0
+
+r = stepFunction(s)
